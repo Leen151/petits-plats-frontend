2748555082027b52867c305ad9bd58f6c1e8deee
Leen151 <leen.m@orange.fr>
2024-05-09 16:24:03 +0200

    intégration des cards recette
    +css (bootstrap + personalisation) - in progress

--- scripts/template.js
+++ scripts/template.js
@@ -1,18 +1,53 @@
 function recipeCard(recipe) {
 
+	// balise article (conteneur)
 	const wrapper = document.createElement("article");
 	wrapper.classList.add("recipe");
+	wrapper.classList.add("card");
+
+	//création de la liste des ingrédients
+	const ingredientsList = document.createElement("ul");
+	ingredientsList.classList.add("ingredients-list");
+
+	recipe.ingredients.forEach(ingredient => {
+		const quantityText = ingredient.quantity ? `${ingredient.quantity}` : '';
+		const unitText = ingredient.unit ?( ingredient.unit === 'grammes' ? 'gr' : ingredient.unit ) : ""
+
+		const listItem = document.createElement("li");
+		listItem.innerHTML = `
+			<p class="ingredient">${ingredient.ingredient}</p>
+			<p class="quantity">${quantityText} ${unitText}</p>
+		`;
+		ingredientsList.appendChild(listItem);
+	});
 
 
 	const content =  `
-	<div>		
-		${recipe.name}
+
+	<img src="/assets/recettes/${recipe.image}" class="card-img-top" alt="${recipe.name}" height="150px" width="150px">
+
+	<div class="card-body">
+		<div class="text-recipe-container">
+			<h2>${recipe.name}</h2>
+			<div class="recipe-description">
+				<h3>recette</h3>
+				<p>${recipe.description}</p>
+			</div>
+			<div class="recipe-ingredients">
+				<h3>ingredients</h3>		
+				${ingredientsList.outerHTML}
+			</div>
+			
+		</div>	
+
+		<p>${recipe.time}min</p>
 	</div>
 	`;
 
 	wrapper.innerHTML = content
 	return wrapper
-
-
 }
+
+
+
 export{recipeCard}
