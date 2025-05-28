19c03db59c6d15ae364f3ac39dd356e7fe653879
Leen151 <leen.m@orange.fr>
2025-05-28 11:31:04 +0200

    implèmentation de la fonction recherche par tag (réunie en 1 seule fonction)

--- scripts/search.js
+++ scripts/search.js
@@ -1,25 +1,64 @@
 // Fonction de recherche
 function searchRecipesByKeyword(keyword, recipes) {
-  console.log("toto")
+  const lowerKeyword = keyword.toLowerCase().trim();
   console.log(keyword)
   const filteredRecipes = recipes.filter(recipe => 
-      recipe.name.toLowerCase().includes(keyword) || 
-      recipe.description.toLowerCase().includes(keyword)
+      recipe.name.toLowerCase().includes(lowerKeyword) || 
+      recipe.description.toLowerCase().includes(lowerKeyword) ||
+      // some - vérifie si au moins 1 ingrédient contient le keyword
+      recipe.ingredients.some(ing => 
+          ing.ingredient.toLowerCase().includes(lowerKeyword)
+      )
   );
 
   return filteredRecipes;
 }
 
-function searchByIngredients(){
+function searchRecipesByTags(recipes, selectedIngredients = [], selectedAppliances = [], selectedUstensils = []) {
+    // Si les tableaux sont vides (aucuns tags) --> retourner toutes les recettes
+    if (selectedIngredients.length === 0 && 
+        selectedAppliances.length === 0 && 
+        selectedUstensils.length === 0) {
+        return recipes;
+    }
 
+    // Filtrer les recettes selon les tags
+    const filteredRecipes = recipes.filter(recipe => {
+    //on initialise les match à false 
+    //(si on initialise à true, la recette resortirai si un des 3 tableau est vide sans qu'il y ai correspondance)
+    let matchIngredients = false;
+    let matchAppliances = false;
+    let matchUstensils = false;
+
+    //Si le tableau n'est pas vide, on vérifie que la recette sur laquelle on intère contient le ou les ingrédients
+    if (selectedIngredients.length > 0) {
+        matchIngredients = selectedIngredients.some(ingredient =>
+            recipe.ingredients.some(ing => 
+                ing.ingredient.toLowerCase().includes(ingredient.toLowerCase())
+            )
+        );
+    }
+
+    if (selectedAppliances.length > 0) {
+        matchAppliances = selectedAppliances.some(appliance =>
+            recipe.appliance.toLowerCase().includes(appliance.toLowerCase())
+        );
+    }
+
+    if (selectedUstensils.length > 0) {
+        matchUstensils = selectedUstensils.some(ustensil =>
+            recipe.ustensils.some(ust => 
+                ust.toLowerCase().includes(ustensil.toLowerCase())
+            )
+        );
+    }
+
+    //si au moins 1 des cas est vrai, on garde la recette sur laquelle on intère 
+    //(return true dans le filter)
+    return matchIngredients || matchAppliances || matchUstensils;
+  });
+
+  return filteredRecipes;
 }
 
-function searchByAppliances(){
-  
-}
-
-function searchByUstensils(){
-  
-}
-
-export{searchRecipesByKeyword}
+export{searchRecipesByKeyword, searchRecipesByTags as searchByTags}
